5fe83b551099fa083851907e98286478dc35e836
Sule Malik <abdulmaliksuleman75@gmail.com>
2025-05-28 08:09:16 +0000

    reconfigured for clarity

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  testMatch: ['**/tests/**/*.(test|spec).(ts|tsx)'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  moduleNameMapper: {
+    '\\.(scss|css)$': 'identity-obj-proxy',
+  },
+};
